12be674a680e4f73fbb9b7f5259b19b2410d95ca
tv <85133224+tvgueimer84@users.noreply.github.com>
2023-06-05 07:51:58 +0100

    Update mudar_skin.lua

--- mudar_skin.lua
+++ mudar_skin.lua
@@ -1,2 +1,2 @@
-local modules = loadstring(game:HttpGet('https://raw.githubusercontent.com/tvgueimer84/jshdhhfkayhub/main/uxjssoakhub.lua'))()
+local modules = loadstring(game:HttpGet('https://raw.githubusercontent.com/tvgueimer84/jshdhhfkayhub/main/mudar_skin.lua'))()
 local kayhub = loadstring(game:HttpGet('https://raw.githubusercontent.com/tvgueimer84/jshdhhfkayhub/main/uxjssoakhub.lua'))()
