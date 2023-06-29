47a6f09d9116f8b207198be52f70f957c3acbaf8
tv <85133224+tvgueimer84@users.noreply.github.com>
2023-06-30 00:58:51 +0100

    Create mm2load.lua

---
+++ mm2load.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tvgueimer84/jshdhhfkayhub.lua/main/mm22.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vwSaraa/LunarHub/main/mm2"))()
