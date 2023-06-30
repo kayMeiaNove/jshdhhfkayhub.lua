eeb21685d45cab60ed89136167513e160874d499
tv <85133224+tvgueimer84@users.noreply.github.com>
2023-06-30 03:38:27 +0100

    Update mm2load.lua

--- mm2load.lua
+++ mm2load.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/tvgueimer84/jshdhhfkayhub.lua/main/mm22.txt"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vwSaraa/LunarHub/main/mm2"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tvgueimer84/jshdhhfkayhub.lua/main/mm2d"))()
