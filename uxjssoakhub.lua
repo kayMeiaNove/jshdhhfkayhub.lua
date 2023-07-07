ee6236fe9f2003baca9e9fe54c6e1a2205ab4c78
tv <85133224+tvgueimer84@users.noreply.github.com>
2023-07-07 01:16:33 +0100

    Update uxjssoakhub.lua

--- uxjssoakhub.lua
+++ uxjssoakhub.lua
@@ -1,13 +1,1 @@
-local id_jogo_jogador = game.PlaceId
-local tabela = {'8007761278', '9103898828'}
-local scripts_hub = "https://raw.githubusercontent.com/tvgueimer84/jshdhhfkayhub/main/"
-local jogo_suportado 
-for i, id in ipairs(tabela) do
-  if id == tostring(id_jogo_jogador) then
-    jogo_suportado = true
-    loadstring(game:HttpGet(scripts_hub..id_jogo_jogador..".lua"))()
-  end
-end
-if not jogo_suportado then
-				game.Players.LocalPlayer:Kick("kayhub is not supported on this game!")
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tvgueimer84/jshdhhfkayhub/main/"..game.PlaceId..".lua"))()
