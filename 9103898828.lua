77d03f914c63bf6cd9da39ae13f1eef112718e07
tv <85133224+tvgueimer84@users.noreply.github.com>
2023-07-18 02:06:22 +0100

    Update 9103898828.lua

--- 9103898828.lua
+++ 9103898828.lua
@@ -1,1 +1,1 @@
-game:GetService('Players').LocalPlayer:Kick('this is script will be updated soon, if you use it now you will be banned by anticheat')
+game:GetService('Players').LocalPlayer:Kick('this  script will be updated soon, if you use it now you will be banned by anticheat')
